3c173c6c9f9b76589877fd770bd140c6276d9755
Adam <aevancho@me.com>
2021-02-04 16:29:30 -0500

    added icons to contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,23 +10,24 @@ class Contact extends Component {
     return (
       <div id="contact-section">
         <form className="pt-5">
-            <h1 className="title text-center mb-5">Let's Talk Turkey.</h1>
+            <h1 className="title text-center mb-5">Let's talk turkey.</h1>
 
                 <div className="form-group position-relative">
                     <label htmlFor="formName" className="d-block">
-                        <i className="icon" data-feather="user"></i>
+                        <i id="userIcon" className="far fa-user"></i>
                     </label>
                     <input type="text" id="formName" className="form-control form-control-lg" placeholder="Name"></input>
                 </div>
 
                 <div className="form-group position-relative">
                     <label htmlFor="formEmail" className="d-block">
-                        <i className="icon" data-feather="mail"></i>
+                        <i id="mailIcon" className="fas fa-at"></i>
                     </label>
                     <input type="email" id="formEmail" className="form-control form-control-lg" placeholder="E-mail"></input>
                 </div>
 
-                <div className="form-group message">
+                <div className="form-group message position-relative">
+                <i id="messageIcon2" className="far fa-comment"></i>
                     <textarea id="formMessage" className="form-control form-control-lg" rows="7" placeholder="Message"></textarea>
                 </div>
             
